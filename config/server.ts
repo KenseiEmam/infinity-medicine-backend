8c95a969b6d6539943a62636dee8c9ad4903b58b
unknown <mohamed.ashraf2k@gmail.com>
2026-01-09 17:57:03 +0400

    change server.ts

--- config/server.ts
+++ config/server.ts
@@ -3,10 +3,15 @@ import path from 'path';
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+
+  url: env('PUBLIC_URL'),
+
+  proxy: true,
+
   app: {
     keys: env.array('APP_KEYS'),
   },
-  // Redirect Strapi temp folder to project-local ./temp
+
   paths: {
     temp: path.resolve(__dirname, '..', 'temp'),
   },
