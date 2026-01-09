a1cda6eb7bc962d887f95c09c10164974c9ec092
unknown <mohamed.ashraf2k@gmail.com>
2026-01-09 16:14:03 +0400

    change provider

--- config/plugins.ts
+++ config/plugins.ts
@@ -2,20 +2,11 @@ module.exports = ({ env }) => ({
   // Upload plugin configuration
   upload: {
     config: {
-      provider: 'local', // use the local provider
+      provider: 'cloudinary',
       providerOptions: {
-        sizeLimit: 100000000, // optional: max file size in bytes (100 MB here)
-      },
-      actionOptions: {
-        upload: {
-          // optional: configure upload behavior
-        },
-        delete: {},
-      },
-      // Force all uploads into ./public/uploads
-      localServer: {
-        maxAge: 3600000, // cache age in ms for serving files
-        path: 'public/uploads', // all uploaded files go here
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
       },
     },
   },
