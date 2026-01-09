e3014d31b7a9c5caa668db1bfb144daeeeacbc4b
unknown <mohamed.ashraf2k@gmail.com>
2026-01-09 19:05:03 +0400

    updated middleware

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -26,7 +26,17 @@ export default [
   'strapi::poweredBy',
   'strapi::logger',
   'strapi::query',
-  'strapi::body',
+  {
+    name: 'strapi::body',
+    config: {
+      jsonLimit: '50mb', // <-- increase limit for imports/exports
+      formLimit: '50mb',
+      textLimit: '50mb',
+      formidable: {
+        maxFileSize: 50 * 1024 * 1024, // 50MB
+      },
+    },
+  },
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
